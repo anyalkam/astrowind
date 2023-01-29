ff61cc0511a14f8246ae4bd52483e2e3f1b11381
m.darmiche <m.darmiche@coozzy.ch>
2023-01-29 03:30:57 +0100

    channge to ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ export default defineConfig({
   base: SITE.basePathname,
   trailingSlash: SITE.trailingSlash ? 'always' : 'never',
 
-  output: 'static',
+  output: 'server',
 
   integrations: [
     tailwind({
